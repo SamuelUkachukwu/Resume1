113a7104d14fbeb7402c229d9656666af0523eca
SamuelUkachukwu <shybold@yahoo.com>
2022-08-14 14:53:58 +0000

    Add templates and html elements for base and index templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
+from .models import Project, About
 
-# Create your views here.
+
+class Home(View):
+    """This generic based view, list the titles of projects
+    and details about me"""
+    def get(self, request, *args, **kwargs):
+        projects = Project.objects.all().order_by("-pk")
+        queryset = About.objects.filter(pk=1)
+        about = get_object_or_404(queryset, pk=1)
+
+        return render(
+            request,
+            "index.html",
+            {
+                "projects": projects,
+                'about': about,
+            },
+        )
