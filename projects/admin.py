66f276fa925b4cafe9490dfeb4c75533481cdd63
SamuelUkachukwu <shybold@yahoo.com>
2022-08-14 14:42:50 +0000

    Add database models

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'languages')
+    list_filter = ('title',)
+    search_fields = ['title', 'languages', 'github_link']
+
+
+@admin.register(models.About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'twitter',
+        'github',
+        'linkedin',
+        'email',
+        'phone_number'
+        )
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from multiselectfield import MultiSelectField
+
+
+LANG_CHOICE = (
+    ('html', 'HTML-5'),
+    ('css', 'CSS-3'),
+    ('python', 'Python'),
+    ('javascript', 'JavaScript'),
+    ('django', 'Django'),
+    ('flask', 'Flask'),
+    ('bootstrap', 'Bootstrap')
+    )
+
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=30, unique=True,)
+    languages = MultiSelectField(choices=LANG_CHOICE)
+    project_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(max_length=550, blank=True)
+    github_link = models.CharField(max_length=200)
+    host_link = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class About(models.Model):
+    name = models.CharField(max_length=30)
+    twitter = models.CharField(max_length=30)
+    github = models.CharField(max_length=30)
+    linkedin = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    profession = models.TextField(max_length=400)
+    skills = models.TextField(max_length=400)
+    engage_me = models.TextField(max_length=400)
+    about_image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.name
